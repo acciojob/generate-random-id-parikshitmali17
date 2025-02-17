f0e86696928ccb5e1ab9fd4ff8d8b1c7c32fc437
Parikshit Mali <parikshitmali17@gmail.com>
2025-02-17 17:57:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 function makeid(l) {
   // write your code here
+	let s=""
+	let char_list=`ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz012
+		3456789`
+	
+	if(l<10){
+		for(let i=0 ;i<=l; i++){
+			s=charAt(Math.floor(Math.random()*char_list.length))+s
+		}
+	}
+	return s
+
+
 }
 
 // Do not change the code below.
